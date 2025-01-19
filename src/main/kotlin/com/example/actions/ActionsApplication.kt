88dd54cf2f4e6db856f5d572c0226eb89067649c
zcaiobs <zcaiobernardodasilva@gmail.com>
2025-01-19 15:57:59 -0300

    first commit

---
+++ src/main/kotlin/com/example/actions/ActionsApplication.kt
@@ -0,0 +1,12 @@
+package com.example.actions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActionsApplication>(*args)
+	println("Hello World")
+}
---
+++ src/test/kotlin/com/example/actions/ActionsApplicationTests.kt
@@ -0,0 +1,19 @@
+package com.example.actions
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+	@Test
+	fun teste() {
+		Assertions.assertEquals(1, 1)
+	}
+
+}
